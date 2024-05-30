3effff9b2921a0f10fae62f215da675462238a41
artur tenca <arturtenca1@gmail.com>
2024-05-30 02:35:48 -0300

    criando botao

--- GlobalSolution/src/js/script.js
+++ GlobalSolution/src/js/script.js
@@ -1,1 +1,10 @@
-//menu hamburguer
+//esconder as informações da empresa
+document.getElementById('botão1').addEventListener('click', function() {
+    var section = document.getElementById('sec1');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
+
