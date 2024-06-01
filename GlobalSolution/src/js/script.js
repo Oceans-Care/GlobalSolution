7234a465d749a5a2498a081a25dc35a2c586128d
artur tenca <arturtenca1@gmail.com>
2024-06-01 00:49:08 -0300

    fazendo terceira section

--- GlobalSolution/src/js/script.js
+++ GlobalSolution/src/js/script.js
@@ -8,3 +8,29 @@ document.getElementById('showSectionButton').addEventListener('click', function(
         hiddenSection.classList.add('hidden');
     }
 });
+
+// SlideShow Automático
+let imagens=['./src/img/grafico1.png','./src/img/garfico2.png','./src/img/grafico3.png'];
+let index = 0;
+let time = 5000;
+
+function slideShow(){
+    document.getElementById('imgBanner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
+
+document.getElementById('aparecergraficos').addEventListener('click', function() {
+
+    var hiddenSection = document.getElementById('banner');
+    if (hiddenSection.classList.contains('hidden')) {
+        hiddenSection.classList.remove('hidden');
+    } else {
+        hiddenSection.classList.add('hidden');
+    }
+});
